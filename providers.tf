22b089cccf79c25c442d2d1c49e5f668de421534
nbalan4u <68507961+nbalan4u@users.noreply.github.com>
2023-11-12 00:44:19 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,5 @@
+variable "client_secret" {
+}
 terraform {
   required_providers {
     azurerm = {
@@ -13,4 +15,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+client_id = "a23932e6-0c35-4bd3-af50-9ff80b458e51"
+client_secret = var.client_secret
+tenant_id = "4290e7da-7cb6-4993-87c4-4e8cf3315bba"
+subscription_id = "69dd90d1-d145-429d-98e6-6ea267c3d5c4"
 }
