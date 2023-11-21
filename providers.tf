1bb64af631f7583a9c4eda52201e66b1b4c0d18d
nbalan4u <68507961+nbalan4u@users.noreply.github.com>
2023-11-21 16:06:06 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -18,7 +18,6 @@ client_id = "a23932e6-0c35-4bd3-af50-9ff80b458e51"
 client_secret = "Ia28Q~hIVM~fs9esmi5_YYRpmeybGkxnBJ4YKcOI"
 tenant_id = "4290e7da-7cb6-4993-87c4-4e8cf3315bba"
 subscription_id = "69dd90d1-d145-429d-98e6-6ea267c3d5c4"
-}
 
     backend "azurerm" {
     resource_group_name  = "Test_RG"
@@ -27,6 +26,5 @@ subscription_id = "69dd90d1-d145-429d-98e6-6ea267c3d5c4"
     key                  = "terraform.tfstate"
     use_azuread_auth     = "true"
   }
-provider "azurerm" {
-  features {}
+
 }
